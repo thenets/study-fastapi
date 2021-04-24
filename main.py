8015ca9b75715e49c14c51aea6724dda5019fd2b
Luiz Costa <luiz@thenets.org>
2021-04-24 08:08:14 -0300

    Path examples

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    return {"item_id": item_id, "times_two": str(int(item_id)*2)}
+
+@app.get("/items/{item_id}/configs/{config_id}")
+async def read_item(item_id, config_id):
+    return {"item_id": item_id, "times_two": str(int(item_id)*2), "config_id": config_id}
